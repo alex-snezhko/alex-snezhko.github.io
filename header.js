cc77a8aa20137f44e1e936bf270f10ad5b22c0e4
Alex Snezhko <alexsnezhko89@gmail.com>
2020-03-24 18:30:22 -0400

    completed projects section

--- header.js
+++ header.js
@@ -1,44 +1,54 @@
-var header = $("#header");
-var navbar = $("#navbar");
-var titleBar = $("#titlebar");
-var links = $("#header-links");
-var title = $("#title");
+$(function() {
+    var header = $("header");
+    var navbar = $("#navbar");
 
-var navbarY = titleBar.get(0).offsetHeight;
-var h = navbar.get(0).offsetHeight;
-// spacer for making sure navbar is cleared when at top of page
-var spacer = $("<div></div>").height(navbar.get(0).offsetHeight + "px");
+    var navbarY = $("#titlebar")[0].offsetHeight;
+    var h = navbar[0].offsetHeight;
+    // spacer for making sure navbar is cleared when at top of page
+    var spacer = $("<div></div>").height(h + "px");
 
-var atTop = false;
-function nav() {
-    if (window.pageYOffset >= navbarY) {
-        if (!atTop) {
-            navbar.addClass("at-top");
-            header.append(spacer);
-            atTop = true;
+    var navAbout = $("#nav-about");
+    var navSkills = $("#nav-skills");
+    var navProjects = $("#nav-projects");
+
+    var skillsY = $("#skills")[0].offsetTop;
+    var projectsY = $("#projects")[0].offsetTop;
+
+    var atTop = false;
+    function nav() {
+        var y = window.pageYOffset;
+        if (y >= navbarY) {
+            if (!atTop) {
+                navbar.addClass("at-top");
+                header.append(spacer);
+                atTop = true;
+            }
+        } else {
+            if (atTop) {
+                navbar.removeClass("at-top");
+                spacer.remove();
+                atTop = false;
+            }
         }
-    } else {
-        if (atTop) {
-            navbar.removeClass("at-top");
-            spacer.remove();
-            atTop = false;
+
+        var sub = atTop ? h : 0;
+        if (sub === h) {
+            asdf = 3;
+        }
+        if (y >= projectsY - sub) {
+            navAbout.removeClass("thispage");
+            navSkills.removeClass("thispage");
+            navProjects.addClass("thispage");
+        } else if (y >= skillsY - sub) {
+            navAbout.removeClass("thispage");
+            navSkills.addClass("thispage");
+            navProjects.removeClass("thispage");
+        } else {
+            navAbout.addClass("thispage");
+            navSkills.removeClass("thispage");
+            navProjects.removeClass("thispage");
         }
     }
-}
-window.addEventListener("scroll", nav);
-
-/*function shiftContactInfo() {
-    titleBar.css("flex-flow", "row");
-    contactBox.css("text-align", "right");
-    if (contactBox.offset().top > title.offset().top) {
-        titleBar.css("flex-flow", "column");
-        contactBox.css("text-align", "center");
-    } else {
-        titleBar.css("flex-flow", "row");
-        contactBox.css("text-align", "right");
-    }
-
-    navbarY = titleBar.height();
-}
-shiftContactInfo();
-window.addEventListener("resize", shiftContactInfo);*/
+    $(window).on("scroll", nav);
+    nav();
+});
