fa2c0b877f0bf17d74332aa2cb375d85f76a1c84
Alex Snezhko <alexsnezhko89@gmail.com>
2022-11-13 18:31:58 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://alex-snezhko.github.io"
+  site: "https://www.alexsnezhko.com"
 });
